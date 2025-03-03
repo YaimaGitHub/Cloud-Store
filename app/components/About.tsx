608a3ce33a1d76b345c7e187ce3a9a2b3a593393
YaimaGitHub <tvalacarta33@gmail.com>
2025-03-02 23:16:37 -0500

    ok
    ok

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -7,12 +7,14 @@ const About = () => {
     <div className="bg-zinc-100 dark:bg-zinc-900/50 text-center py-16">
       <div className="w-[min(1200px,100%-4rem)] mx-auto">
         <h2 className="text-5xl max-md:text-3xl font-bold mb-5">
-          Had enough of boring dummy data?
+        Desde un Start-up hasta ser el fabricante global
         </h2>
         <p className="text-lg text-gray-500 w-[min(1000px,100%)] mx-auto">
-          We offer the finest free products API available. We've carefully
-          selected top-notch product thumbnails and details to ensure websites
-          built using our API stand out ✨.
+        Nuestra cultura corporativa se basa en un principio: Fundado en el Compromiso. Estamos orgullosos 
+        de los productos que construimos con la ayuda de la experiencia de nuestros empleados, para garantizar 
+        fiabilidad y la compatibilidad de los sitios web's. 
+        ✨Nuestros productos hacen posible el mundo de hoy y acercan el mundo del mañana.✨
+                         ✨ Estámos contigo a cada paso del camino✨
         </p>
       </div>
 
@@ -20,7 +22,7 @@ const About = () => {
         <Link href="https://shop-vert-eight.vercel.app/" target="_blank">
           <div className="w-[min(768px,100%-4rem)] aspect-video relative mx-auto shadow-md dark:shadow-white/20 group overflow-hidden rounded-xl hover:rounded-3xl transition-all hover:shadow-xl">
             <Image
-              src={showcase}
+              src="/profile.png"
               className="h-full w-full object-contain group-hover:scale-[1.02] transition-transform"
               alt="Illustration"
               priority

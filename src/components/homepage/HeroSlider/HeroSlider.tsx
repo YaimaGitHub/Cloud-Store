1093cc008631465792748a708be344c1212978b7
YaimaGitHub <tvalacarta33@gmail.com>
2025-03-04 23:20:49 -0500

    ok
    ok

--- src/components/homepage/HeroSlider/HeroSlider.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Interface from "./Interface";
-
-export default function HeroSlider() {
-  return <Interface />
-}
--- src/components/shared/HandleImageLoad.js
+++
@@ -1,4 +0,0 @@
-export default function HandleImageLoad(e) {
-  //when image finishes loading, creates a smooth opacity initial animation
-  e.currentTarget.classList.remove("opacity-0");
-}

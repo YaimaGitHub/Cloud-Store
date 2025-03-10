08f6f0f39489e604f49d1cc229c76ed513d17885
YaimaGitHub <tvalacarta33@gmail.com>
2025-03-10 05:23:49 -0400

    ok
    ok

--- src/components/commons/Footer.jsx
+++ src/components/commons/Footer.jsx
@@ -8,11 +8,11 @@ function Footer() {
 
         <Stack isInline>
         <a href="https://www.instagram.com/" class="btn btn-sm btn-white btn-social mr-3">
-            <Icon name="link" mx="2" />
+            <Icon name="phone" mx="2" />
             <i class="fab fa-instagram"></i>
           </a>
           <a href="https://www.facebook.com/" target="_blank" class="btn btn-sm btn-white btn-social mr-3">
-            <Icon name="link" mx="2" />
+            <Icon name="email" mx="2" />
           </a>
           <a href="https://api.whatsapp.com/send?phone=5354690878" target="_blank" class="btn btn-sm btn-white btn-social mr-3" >
             <Icon name="link" mx="2" />

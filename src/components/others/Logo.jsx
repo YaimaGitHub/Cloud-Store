3546572b1cbd8b196bcd9c9e0ede4fb23f226594
YaimaGitHub <tvalacarta33@gmail.com>
2025-03-25 07:42:06 -0400

    ok
    ok

--- src/components/others/Logo.jsx
+++ src/components/others/Logo.jsx
@@ -1,8 +1,8 @@
 export default function Logo({ width = 100 }) {
   return (
-<svg height="60" width="200" xmlns="http://www.w3.org/2000/svg">
+<svg height="60" width="100" xmlns="http://www.w3.org/2000/svg">
 
-  <image x="-110" y="-10" width="300" height="80" href="https://120000.s3.us-east-005.backblazeb2.com/51437.png" />
+  <image x="-120" y="-8" width="300" height="70" href="https://120000.s3.us-east-005.backblazeb2.com/51437.png" />
 
   
 
